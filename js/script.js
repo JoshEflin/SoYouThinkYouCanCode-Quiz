fe73b37dd33095cd0dabf0825622257672a05f4a
ragingpenguin <eflinjh@gmail.com>
2022-12-14 22:51:22 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+
+var beginQuiz = document.querySelector(".begin")
+console.log(beginQuiz)
+// create a quiz function
+
+ function bestQuiz() {
+
+
+ }
+// quiz function  contains  a set of questions
+// quiz needs a time interval countdown
+//  The next question (item in questions object) is an event that occurs when ANY answer is selected
+// Questions object asks questions in a random order
+// clicking the correct question  adds 1 to a count variable measuring correct answers
+
+
+
+// clicking an incorrect questions reduces time remaining in quiz
+
+
+
+
+// when quiz is complete, score is logged to local storage
+// 
+
+
+// add an event listener for button click event to start quiz
