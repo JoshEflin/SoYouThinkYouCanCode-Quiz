3a1de24b2a46b945dfb133e482765fa1e431cba5
ragingpenguin <eflinjh@gmail.com>
2022-12-16 13:07:10 -0500

    used a for loop for incorrect answers

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
+
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
 // WHEN all questions are answered or the timer reaches 0
@@ -11,11 +7,13 @@
 // THEN I can save my initials and my score
 
 // create all Global variables
-var timeRemaining = 10;
+var timeRemaining = 100;
 var score = 0;
 var quizBtn = document.querySelector(".begin");
 var timerEl = document.querySelector(".timer");
 var gameArena = document.querySelector(".game-arena");
+var timer;
+var scoreEl = document.createElement("p")
 
 // created HTML elements to be appended in later function
 var questionAsked = document.createElement("div");
@@ -25,26 +23,26 @@ var otherAnswer1 = document.createElement("li");
 var otherAnswer2 = document.createElement("li");
 var otherAnswer3 = document.createElement("li");
 
-// create an array of question-answer objects
-var quizObjects = [{
-    question: "How many cups of coffee are in one JavaScript?",
-    correct: "JavaScript is a programming language and has nothing to do with coffee",
+// create an array of question-answer obiects
+var quizObiects = [{
+    question: "How many cups of coffee are in one iavaScript?",
+    correct: "iavaScript is a programming language and has nothing to do with coffee",
     a2: "15!",
-    a3: "None, a JavaScript is a working prototype of a Hollywood screenplay",
-    a4: "One Javascript contains exactly 4 cups of DECAF only"
+    a3: "None, a iavaScript is a working prototype of a Hollywood screenplay",
+    a4: "One iavascript contains exactly 4 cups of DECAF only"
 },
 {
     question: "As a general rule, a develeper should write code so that...",
     correct: "the next developer can easily make sense of its functionality due to detailed comments",
     a2: "the next developer has a very difficult time reading it",
-    a3: "it needs to be debugged frequently, leading to job security!",
+    a3: "it needs to be debugged frequently, leading to iob security!",
     a4: "do NOT click me"
 },
 {
-    question: "Javascript doesnt not need a compiler to run because V8 engine renders the Javascript to the webpage....",
-    correct: "Just in Time",
+    question: "iavascript doesnt not need a compiler to run because V8 engine renders the iavascript to the webpage....",
+    correct: "iust in Time",
     a2: "Almost on time",
-    a3: "Just a little late",
+    a3: "iust a little late",
     a4: "It doesnt",
 
 },
@@ -68,21 +66,21 @@ var index = 0;
 function endQuiz() {
     //  replaces game-arena with score arena.
     // inside score arena, append a form to the html and store its values in local storage 
-    timeRemaining = 0;
+    clearInterval(timer)
     console.log("we made it to the end")
+    gameArena.removeChild(scoreEl, questionAsked,answerList)
     var scoreboard = document.createElement("div");
     scoreboard.setAttribute("class", "scoreboard")
-    scoreboard.textContent= "WORKING"
+    scoreboard.textContent = "You got "+ score +" answers correct!"
     gameArena.appendChild(scoreboard)
 };
 // create a timer functionality
 function countdown() {
-    
 
-    var timer = setInterval(function () {
+    timer = setInterval(function () {
         timeRemaining--;
         timerEl.textContent = timeRemaining + " seconds left";
-        if (timeRemaining === 0) {
+        if (timeRemaining <= 0) {
             console.log(score)
             clearInterval(timer);
             endQuiz()
@@ -91,77 +89,88 @@ function countdown() {
     }, 1000)
 }
 
-function replaceGameArena(){
+function replaceGameArena() {
     gameArena.removeChild(questionAsked)
     gameArena.removeChild(answerList)
 }
-// function to take in a quiz object from the array and  append all values to HTML based on the values key.
+// function to take in a quiz obiect from the array and  append all values to HTML based on the values key.
 function nextQuestion() {
- index++
- replaceGameArena()
- questionpicker()
+    index++
+    replaceGameArena()
+    questionpicker()
 }
+// the Shuffler function is a basic shuffling algorithm,  replacing one item in an array with a random item and looping through
+function shuffler(answerArray) {
+    var m = answerArray.length, t, i;
 
+    // While there remain elements to shuffle…
+    while (m) {
 
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = answerArray[m];
+        answerArray[m] = answerArray[i];
+        answerArray[i] = t;
+    }
+
+    return answerArray;
+}
 function questionpicker() {
-    var quizObject = quizObjects[index];
-    console.log(quizObject)
-    if (index>= 5 || timeRemaining===0){
+    var quizObiect = quizObiects[index];
+    console.log(quizObiect)
+    if (index >= 5 || timeRemaining === 0) {
         endQuiz()
     } else {
 
-    // give  elements content and set the class attribute 
+        // give  elements content and set the class attribute 
+
+        var answerArray = [correctAnswer, otherAnswer1, otherAnswer2, otherAnswer3];
+
+        questionAsked.textContent = quizObiect.question;
+        questionAsked.setAttribute("class", "question");
+        gameArena.appendChild(questionAsked)
+
+        answerList.setAttribute("class", "answer-list")
+        gameArena.appendChild(answerList);
+
+        correctAnswer.textContent = quizObiect.correct;
+        correctAnswer.setAttribute("class", "correct");
 
 
-    questionAsked.textContent = quizObject.question;
-    questionAsked.setAttribute("class", "question");
-    gameArena.appendChild(questionAsked)
-    
-    answerList.setAttribute("class", "answer-list")
-    gameArena.appendChild(answerList);
-    
-    correctAnswer.textContent = quizObject.correct;
-    correctAnswer.setAttribute("class", "correct");
-    answerList.appendChild(correctAnswer);
-    
-    otherAnswer1.textContent = quizObject.a2;
-    otherAnswer1.setAttribute("class", "incorrect");
-    answerList.appendChild(otherAnswer1);
-    
-    otherAnswer2.textContent = quizObject.a3;
-    otherAnswer2.setAttribute("class", "incorrect")
-    answerList.appendChild(otherAnswer2);
-    
-    otherAnswer3.textContent = quizObject.a4;
-    otherAnswer3.setAttribute("class", "incorrect")
-    answerList.appendChild(otherAnswer3);
+        otherAnswer1.textContent = quizObiect.a2;
+        otherAnswer1.setAttribute("class", "incorrect");
 
 
-    var answerClicked = document.querySelector(".answer-list")
+        otherAnswer2.textContent = quizObiect.a3;
+        otherAnswer2.setAttribute("class", "incorrect")
 
-    console.log(answerClicked)
-    answerClicked.addEventListener("click", nextQuestion)
+
+        otherAnswer3.textContent = quizObiect.a4;
+        otherAnswer3.setAttribute("class", "incorrect")
+
+        shuffler(answerArray)
+        for (i = 0; i < answerArray.length; i++) {
+            answerList.appendChild(answerArray[i])
+        }
+        var incorrectClicked = document.querySelectorAll(".incorrect");
+        var correctClicked = document.querySelector(".correct");
+
+        correctClicked.addEventListener("click", function (){
+            score += 1
+            gameArena.appendChild(scoreEl);
+            scoreEl.textContent = score +" correct answers";
+            nextQuestion();
+        })
+        for(i=0; i<incorrectClicked.length; i++){
+            incorrectClicked[i].addEventListener("click", function(){
+                timeRemaining -= 5;
+                nextQuestion();
+            })
+        }     
     }
-
 }
-
-
-// the correct answer key will listen for a click or a keyboard event that will add to the score counter... 
-
-// if correct
-
-
-// else
-// the wrong answer key will subtract valued from timer element or increase the rate of countdown?
-
-
-
-//  The next question (item in questions object) is an triggered by an event listenerfor when ANY answer is selected
-
-// clicking the correct question  adds 1 to a count variable measuring correct answers
-
-// clicking an incorrect questions reduces time remaining in quiz
-
 function bestQuiz() {
     countdown();
     questionpicker();
@@ -171,3 +180,4 @@ function bestQuiz() {
 // add an event listener for button click event to start quiz
 quizBtn.addEventListener("click", bestQuiz)
 
+// //  array shuffler  (based on Fisher Yates shuffle.)
