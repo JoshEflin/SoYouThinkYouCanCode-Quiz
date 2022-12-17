cbaef6f062ef9b576a5987bfad3b96711175cfec
ragingpenguin <eflinjh@gmail.com>
2022-12-17 15:06:32 -0500

    began working on the function to display scores,  moved variables to global scope (might be a mistake)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
 
@@ -13,7 +9,10 @@ var quizBtn = document.querySelector(".begin");
 var timerEl = document.querySelector(".timer");
 var gameArena = document.querySelector(".game-arena");
 var timer;
-var scoreEl = document.createElement("p")
+var index = 0;
+var userInitials;
+var storedScore;
+
 
 // created HTML elements to be appended in later function
 var questionAsked = document.createElement("div");
@@ -22,32 +21,33 @@ var correctAnswer = document.createElement("li");
 var otherAnswer1 = document.createElement("li");
 var otherAnswer2 = document.createElement("li");
 var otherAnswer3 = document.createElement("li");
+var scoreEl = document.createElement("p");
 
 // create an array of question-answer obiects
 var quizObiects = [{
-    question: "How many cups of coffee are in one iavaScript?",
-    correct: "iavaScript is a programming language and has nothing to do with coffee",
+    question: "How many cups of coffee are in one JavaScript?",
+    correct: "JavaScript is a programming language and has nothing to do with coffee.",
     a2: "15!",
-    a3: "None, a iavaScript is a working prototype of a Hollywood screenplay",
-    a4: "One iavascript contains exactly 4 cups of DECAF only"
+    a3: "None, a JavaScript is a working prototype of a Hollywood screenplay.",
+    a4: "One Javascript contains exactly 4 cups of DECAF only."
 },
 {
     question: "As a general rule, a develeper should write code so that...",
-    correct: "the next developer can easily make sense of its functionality due to detailed comments",
-    a2: "the next developer has a very difficult time reading it",
-    a3: "it needs to be debugged frequently, leading to iob security!",
-    a4: "do NOT click me"
+    correct: "The next developer can easily make sense of its functionality due to detailed comments.",
+    a2: "The next developer has a very difficult time reading it.",
+    a3: "It needs to be debugged frequently, leading to job security!",
+    a4: "Do NOT click me"
 },
 {
-    question: "iavascript doesnt not need a compiler to run because V8 engine renders the iavascript to the webpage....",
-    correct: "iust in Time",
-    a2: "Almost on time",
-    a3: "iust a little late",
-    a4: "It doesnt",
+    question: "Javascript doesn't not need a compiler to run because V8 engine renders the Javascript to the webpage....",
+    correct: "Just in time.",
+    a2: "Almost on time.",
+    a3: "Just a little late",
+    a4: "It doesnt.",
 
 },
 {
-    question: "what does ECMA stand for?",
+    question: "What does ECMA stand for?",
     correct: "European Computer Manufacturers Association",
     a2: "Eichhorn's Christian Ministries Anonymous",
     a3: "Euler's Crazy Mathematical Analysis",
@@ -55,24 +55,46 @@ var quizObiects = [{
 
 },
 {
-    question: "How helpful was this quiz",
+    question: "How helpful was this quiz?",
     correct: "VERY!",
-    a2: "Never make another quiz",
+    a2: "Never make another quiz.",
     a3: "for (each question){let userBrain = mushier};",
-    a4: "This quiz is IMPOSSIBLE"
+    a4: "This quiz is IMPOSSIBLE."
 },];
-var index = 0;
+
 // create a quiz function
 function endQuiz() {
     //  replaces game-arena with score arena.
     // inside score arena, append a form to the html and store its values in local storage 
     clearInterval(timer)
     console.log("we made it to the end")
-    gameArena.removeChild(scoreEl, questionAsked,answerList)
+    gameArena.removeChild(scoreEl, questionAsked, answerList)
     var scoreboard = document.createElement("div");
-    scoreboard.setAttribute("class", "scoreboard")
-    scoreboard.textContent = "You got "+ score +" answers correct!"
-    gameArena.appendChild(scoreboard)
+    scoreboard.setAttribute("class", "scoreboard");
+    scoreboard.textContent = "You got " + score + " answers correct ";
+    timerEl.textContent = "you had " + timeRemaining + " seconds left!";
+    gameArena.appendChild(scoreboard);
+    var initials =  document.createElement("input");
+    gameArena.appendChild(initials)
+    initials.setAttribute("class", "initials")
+    document.querySelector(".initials").addEventListener("keyup", function (evt) {
+        
+        if(evt.key === "Enter"){
+            userInitials=localStorage.setItem("initials",JSON.stringify(initials.value));
+            storedScore = {
+                UI : userInitials,
+                score: score,
+                timerRemaining: timeRemaining,
+            }   
+    }
+        console.log(storedScore)    
+        })
+
+   
+    var storedScores = []
+   
+
+
 };
 // create a timer functionality
 function countdown() {
@@ -81,25 +103,34 @@ function countdown() {
         timeRemaining--;
         timerEl.textContent = timeRemaining + " seconds left";
         if (timeRemaining <= 0) {
-            console.log(score)
             clearInterval(timer);
-            endQuiz()
+            endQuiz();
         }
 
-    }, 1000)
+    }, 1000);
 }
 
 function replaceGameArena() {
-    gameArena.removeChild(questionAsked)
-    gameArena.removeChild(answerList)
+    gameArena.removeChild(questionAsked);
+    gameArena.removeChild(answerList);
 }
 // function to take in a quiz obiect from the array and  append all values to HTML based on the values key.
-function nextQuestion() {
-    index++
-    replaceGameArena()
-    questionpicker()
+function increaseScore() {
+    score += 1;
+    gameArena.appendChild(scoreEl);
+    scoreEl.textContent = score + " correct answers";
+    nextQuestion();
 }
-// the Shuffler function is a basic shuffling algorithm,  replacing one item in an array with a random item and looping through
+function decreaseTime() {
+    timeRemaining -= 5;
+    nextQuestion();
+}
+function nextQuestion() {
+    index++;
+    replaceGameArena();
+    mainQuizFunction();
+}
+// the Shuffler function is a basic shuffling algorithm based on Fisher Yates shuffle. replacing one item in an array with a random item and looping through
 function shuffler(answerArray) {
     var m = answerArray.length, t, i;
 
@@ -117,67 +148,61 @@ function shuffler(answerArray) {
 
     return answerArray;
 }
-function questionpicker() {
+function mainQuizFunction() {
     var quizObiect = quizObiects[index];
-    console.log(quizObiect)
+    console.log(quizObiect);
     if (index >= 5 || timeRemaining === 0) {
-        endQuiz()
+        endQuiz();
     } else {
 
-        // give  elements content and set the class attribute 
+        // give  elements content and set the class attribute. Create an array of all the answers and append them to the HTML in random order.
 
         var answerArray = [correctAnswer, otherAnswer1, otherAnswer2, otherAnswer3];
 
         questionAsked.textContent = quizObiect.question;
         questionAsked.setAttribute("class", "question");
-        gameArena.appendChild(questionAsked)
+        gameArena.appendChild(questionAsked);
 
-        answerList.setAttribute("class", "answer-list")
+        answerList.setAttribute("class", "answer-list");
         gameArena.appendChild(answerList);
 
         correctAnswer.textContent = quizObiect.correct;
         correctAnswer.setAttribute("class", "correct");
 
-
         otherAnswer1.textContent = quizObiect.a2;
         otherAnswer1.setAttribute("class", "incorrect");
 
-
         otherAnswer2.textContent = quizObiect.a3;
-        otherAnswer2.setAttribute("class", "incorrect")
-
+        otherAnswer2.setAttribute("class", "incorrect");
 
         otherAnswer3.textContent = quizObiect.a4;
-        otherAnswer3.setAttribute("class", "incorrect")
+        otherAnswer3.setAttribute("class", "incorrect");
 
-        shuffler(answerArray)
+        shuffler(answerArray);
         for (i = 0; i < answerArray.length; i++) {
-            answerList.appendChild(answerArray[i])
+            answerList.appendChild(answerArray[i]);
         }
+        // create an event listener for correct answers and incorrect answers.  Use a for loop to give the event listener to each list 
+        // element in the array created by querySelectorAll()
         var incorrectClicked = document.querySelectorAll(".incorrect");
         var correctClicked = document.querySelector(".correct");
 
-        correctClicked.addEventListener("click", function (){
-            score += 1
-            gameArena.appendChild(scoreEl);
-            scoreEl.textContent = score +" correct answers";
-            nextQuestion();
-        })
-        for(i=0; i<incorrectClicked.length; i++){
-            incorrectClicked[i].addEventListener("click", function(){
-                timeRemaining -= 5;
-                nextQuestion();
-            })
-        }     
+        correctClicked.addEventListener("click", increaseScore);
+
+        for (i = 0; i < incorrectClicked.length; i++) {
+            incorrectClicked[i].addEventListener("click", decreaseTime);
+
+
+        }
     }
 }
-function bestQuiz() {
+
+function worldsBestQuiz() {
     countdown();
-    questionpicker();
+    mainQuizFunction();
 };
 
-
 // add an event listener for button click event to start quiz
-quizBtn.addEventListener("click", bestQuiz)
+quizBtn.addEventListener("click", worldsBestQuiz);
 
-// //  array shuffler  (based on Fisher Yates shuffle.)
+
